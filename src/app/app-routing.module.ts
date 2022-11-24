56ff1c0427ca145edfa54c3374d8d0f76bad6893
windows <windows@microsoft.com>
2022-11-24 14:36:14 +0530

    add ngbootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { MypageComponent } from './mypage/mypage.component';
+import { MytableComponent } from './mytable/mytable.component';
+import { WallComponent } from './wall/wall.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:AppComponent
+  },
+  {
+    path:'table',
+    component:MytableComponent
+  },
+  {
+    path:'gallery',
+    component:GalleryComponent
+  },
+  {
+    path:'wall',
+    component:WallComponent
+  },
+  {
+    path:'page',
+    component:MypageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { WallComponent } from './wall/wall.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MytableComponent } from './mytable/mytable.component';
+import { MypageComponent } from './mypage/mypage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GalleryComponent,
     WallComponent,
+    MytableComponent,
+    MypageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
