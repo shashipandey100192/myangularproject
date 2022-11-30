0bba69530dd041a2a9d03f257a202e7c6f3e7315
windows <windows@microsoft.com>
2022-11-30 14:32:36 +0530

    use directive

--- src/app/wall/wall.component.ts
+++ src/app/wall/wall.component.ts
@@ -8,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class WallComponent implements OnInit {
 a:string = "dicat india noida";
 b:string = '10/20/2022';
+
+x:number[]=[10,20,50,60,70,80,90,40,50,60,30,20,10];
+
+xyz=[
+  {name:'ravi',age:50},
+  {name:'mohan',age:20},
+  {name:'kumar',age:60},
+  {name:'mohit',age:90},
+  {name:'pankaj',age:20}
+]
+
+
+// c:null=null
+// c:boolean = false;
+// c:boolean = 60>90
+c:boolean = this.x.length>10
   constructor() { }
 
   ngOnInit(): void {
